020171b5a9a8afb5172cf5998a2f953c75655088
AnastasiyaKomissarova <nastulya0483@gmail.com>
2022-07-14 16:54:11 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,23 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let d = b ** 2 - 4 * a * c;
+  let root1;
+  let root2;
+
+  if (d > 0) {
+    root1 = (-b + Math.sqrt(d) ) / (2 * a);
+    root2 = (-b - Math.sqrt(d) ) / (2 * a);
+    arr = [root1, root2];
+  }
+  else if (d === 0) {
+    root1 = -b / (2 * a);
+    arr = [root1];
+  }
+  else if (d < 0) {
+    arr = [];
+  }
   return arr; // array
 }
 
