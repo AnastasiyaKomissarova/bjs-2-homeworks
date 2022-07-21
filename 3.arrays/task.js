e48482f9dad4e9302b43986d546ab8c2036fc763
AnastasiyaKomissarova <nastulya0483@gmail.com>
2022-07-21 15:35:28 +0300

    commit 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length !== arr2.length) { 
+      result = false;
+  }
+  else {
+    result = arr1.every((item, idx) => item === arr2[idx]);
+  }
 
   return result; // boolean
 }
@@ -9,7 +14,9 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
-
+  resultArr = arr
+    .filter((item) => item > 0 && item % 3 === 0)
+    .map((item) => item * 10); 
   return resultArr; // array
 }
+
