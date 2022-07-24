639125965728f8d0340366f470804a152bbe0cac
AnastasiyaKomissarova <nastulya0483@gmail.com>
2022-07-24 21:22:04 +0300

    commit 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
-function Student(name, gender, age) {
-    // Ваш код
-
+function Student (name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+//let student3 = new Student("Anastasia", "femail", 39);
+//let student4 = new Student("Ivan", "male", 35);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) { 
+      this.marks = [];
+  }
+      this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+    marks.forEach (mark => this.addMark(mark));
+}
+
+Student.prototype.getAverage = function () { 
+    let sum = 0;
+    for (let mark of this.marks) {
+        sum += mark;
+    }
+    return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
+}
+
